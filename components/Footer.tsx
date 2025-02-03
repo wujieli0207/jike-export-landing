f80422a4018a584d918c6004b3d63f2b2f6155e0
wujieli <wujieli0207@outlook.com>
2025-02-03 15:37:54 +0800

    feat: 增加 AI Question Answer 链接

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,12 @@ const Footer = () => {
       href: 'https://www.vercelpopular.com/',
       name: 'Vercel Popular',
     },
+    {
+      href: 'https://www.ai-question-answer.com',
+      name: 'AI Question Answer',
+    },
   ]
+
   return (
     <footer className="pt-10">
       <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
